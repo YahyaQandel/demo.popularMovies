e97df7b7a62b4a9d12552401998873224a01efe6
yahya <yahia@beeinteractivemena.com>
2016-09-18 11:53:30 +0200

    code enhancement

--- app/src/main/java/com/udacity/geekless/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/geekless/popularmovies/MainActivity.java
@@ -1,12 +1,9 @@
 package com.udacity.geekless.popularmovies;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.GridView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             fragmentTransaction
-                    .replace(R.id.container, new OfflineActivity())
+                    .replace(R.id.container, new OfflineFragment())
                     .commit();
         }
 
--- app/src/main/java/com/udacity/geekless/popularmovies/OfflineFragment.java
+++ app/src/main/java/com/udacity/geekless/popularmovies/OfflineFragment.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class OfflineActivity extends Fragment {
+public class OfflineFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
